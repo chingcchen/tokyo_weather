4485965d776e235c30a18c2412a55ce6c1c9f1e5
chingcchen <chingche@usc.edu>
2017-08-08 03:36:03 +0000

    Add file: taiwan_weather/taipei/08-Aug-2017.csv

---
+++ taiwan_crawler.py
@@ -0,0 +1,37 @@
+import time
+import pandas as pd
+from time import gmtime, strftime
+
+from git import Repo
+
+
+cities = ['taipei', 'taichung', 'tainan', 'taitung', 'hualien', 'pingtung', 'yilan']
+urls = [
+        'http://www.cwb.gov.tw/V7/observe/24real/Data/46692.htm',
+        'http://www.cwb.gov.tw/V7/observe/24real/Data/46749.htm',
+        'http://www.cwb.gov.tw/V7/observe/24real/Data/46741.htm',
+        'http://www.cwb.gov.tw/V7/observe/24real/Data/46766.htm',
+        'http://www.cwb.gov.tw/V7/observe/24real/Data/46699.htm',
+        'http://www.cwb.gov.tw/V7/observe/24real/Data/C0R17.htm',
+        'http://www.cwb.gov.tw/V7/observe/24real/Data/46708.htm',
+    ]
+    
+for city, url in zip(cities, urls): 
+    df = pd.read_html(url, header=0)
+    df = df[0]
+    del df['溫度(°F>>°C)']
+    fname = "taiwan_weather/" + city + strftime("/%d-%b-%Y.csv", gmtime())
+    df.to_csv(fname)
+
+    repo = Repo('.')
+    origin = repo.remote('origin')
+    file_list = [fname]
+    commit_message = 'Add file: %s' % fname
+    repo.index.add(file_list)
+    repo.index.commit(commit_message)
+    origin.push()
+  
+
+
+
+
